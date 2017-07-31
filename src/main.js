d67fdb2b5d3218a9cd7ef2b31cdf53255818e238
aAxiong <879628926@qq.com>
2017-07-31 17:44:30 +0800

    :bear: 7.28日常修改

--- src/main.js
+++ src/main.js
@@ -2,24 +2,27 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App';
-import Header from './components/header/header';
-import Goods from './components/goods/goods';
-import Ratings from './components/Ratings/Ratings';
-import Seller from './components/Seller/Seller';
+import Goods from 'components/goods/goods';
+import Ratings from 'components/Ratings/Ratings';
+import Seller from 'components/Seller/Seller';
+import 'common/stylus/index.styl';
+
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 Vue.config.productionTip = false
 
 
 const routes = [
-  { path: '/', component: Header },
   { path: '/goods', component: Goods },
   { path: '/ratings', component: Ratings },
   { path: '/seller', component: Seller }
 ]
 
 const router = new VueRouter({
+  linkActiveClass:"active",
   routes // （缩写）相当于 routes: routes
   
 })
@@ -27,6 +30,9 @@ const router = new VueRouter({
 const app = new Vue({
   router,
   render:(h)=>h(App)
+
 }).$mount('#app')
+
+router.push('/goods')
 
 
