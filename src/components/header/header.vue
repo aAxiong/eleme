30cb6586e84b824da17df2180f3fc7cfe8eca209
aAxiong <879628926@qq.com>
2017-08-01 16:33:56 +0800

    :bear:header finish

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -11,12 +11,22 @@
         </div>
         <div class="description">{{seller.description}}/{{seller.deliveryTime}}分钟送达</div>
         <div v-if="seller.supports" class="supports">
-          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
-          <span class="text">{{seller.supports[0].description}}</span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span><span class="text">{{seller.supports[0].description}}</span>
         </div>
+      </div>
+      <div v-if="seller.supports" class="support-count">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
     <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width='100%'>
 
     </div>
   </div>
@@ -29,16 +39,18 @@
         }
       },
       created(){
-        this.classMap['decrease','discount','special','invoice','guarantee'];
+        this.classMap = ['decrease','discount','special','invoice','guarantee']
       }
     };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
 @import "../../common/stylus/mixin.styl"
 .header
+  position:relative
   color:#fff
-  background:#000
+  background:rgba(7,17,27,0.5)
   .content-wrapper
+    position:relative
     padding:24px 12px 18px 24px
     font-size:0px
     .avatar
@@ -70,11 +82,12 @@
       .supports
         .icon
           display :inline-block
+          vertical-align :top
           width:12px
           height:12px
-          margin-left:4px
-          background-size:12px 12px;
-          background-repeat :no-repeat;
+          margin-right:4px
+          background-size:12px 12px
+          background-repeat :no-repeat
           &.decrease
             bg-image('decrease_1')
           &.discount
@@ -85,10 +98,64 @@
             bg-image('invoice_1')
           &.special
             bg-image('special_1')
-          
+        .text
+          line-height:12px
+          font-size:10px
+  .support-count
+    position:absolute
+    right:12px
+    bottom:14px
+    padding:0 8px
+    height:24px
+    line-height:24px
+    border-radius:14px
+    background:rgba(0,0,0,0.2)
+    text-align:center
+    .count
+      vertical-align :top
+      font-size:10px
+    .icon-keyboard_arrow_right
+      line-height:24px
+      margin-left:2px
+      font-size:10px
+  .bulletin-wrapper
+    position:relative
+    height:28px
+    line-height:28px
+    padding:0 22px 0 12px
+    white-space :nowrap
+    overflow :hidden
+    text-overflow :ellipsis
+    background:rgba(7,17,27,0.2)
+    .bulletin-title
+      display :inline-block
+      vertical-align :top
+      width:22px
+      height:12px
+      margin-top:7px
+      bg-image('bulletin')
+      background-size:22px 12px
+      background-repeat :no-repeat
+    .bulletin-text
+      vertical-align :top
+      margin:0 4px
+      font-size:10px
+    .icon-keyboard_arrow_right
+      position:absolute
+      font-size:10px
+      right:12px
+      top:8px
+  .background
+    position:absolute
+    top:0
+    left:0
+    width:100%
+    height:100%
+    z-index:-1
+    filter :blur(10px)
+    img
+      height:100%
 
 
-
-  
 </style>
 
