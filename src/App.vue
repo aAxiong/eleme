2b51ddd0acd9868a4a58cdf41d9e872a9753f389
aAxiong <879628926@qq.com>
2017-08-22 09:44:57 +0800

    wrong

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,45 @@
   <div>
     <v-header :seller="seller"></v-header>
     <div class="tab border-1px">
-        <div class="tab-item"><router-link to="/goods">商品</router-link></div>
-        <div class="tab-item"><router-link to="/ratings">评论</router-link></div>
-        <div class="tab-item"><router-link to="/seller">商家</router-link></div>
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
     </div>
 
-     <!-- 路由出口 -->
-  <!-- 路由匹配到的组件将渲染在这里 -->
-   <router-view :seller="seller"></router-view> 
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view :seller="seller"></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import header from "./components/header/header"
+import header from "./components/header/header"
 
-  const ERR_OK = 0
-  export default{
-    data(){
-      return{
-        seller:{}
-      }
-    },
-    created(){
-      this.$http.get('./api/seller').then((response)=>{
-          response=response.body;
-          if(response.errno===ERR_OK){
-             this.seller=response.data;
-          }
-      });
-    },
-    components:{
-      "v-header":header
+const ERR_OK = 0
+export default {
+  data() {
+    return {
+      seller: {}
     }
+  },
+  created() {
+    this.$http.get('./api/seller').then((response) => {
+      response = response.body;
+      if (response.errno === ERR_OK) {
+        this.seller = response.data;
+      }
+    });
+  },
+  components: {
+    "v-header": header
   }
+}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
